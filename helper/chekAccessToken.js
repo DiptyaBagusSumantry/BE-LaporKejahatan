e0cda2fa6b74225aed14b3d76bc3557f9089a1da
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-01-15 22:54:11 +0700

    Update chekAccessToken.js

--- helper/chekAccessToken.js
+++ helper/chekAccessToken.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 function accesToken(req) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
   const accesToken = jwt.verify(
-    req.cookies.refreshToken,
+    token,
     process.env.REFRESH_TOKEN_SECRET,
     (error, decoded) => {
       if (error) return res.sendStatus(403);
