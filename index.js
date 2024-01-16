cca76012868f868714dace666600012119774383
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-01-16 10:02:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 // app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(
   "/assets/images/laporan",
   express.static(path.join(__dirname, "assets/images/laporan"))
