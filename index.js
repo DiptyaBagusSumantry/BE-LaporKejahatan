9d75b98c91fbbf3e1ed54efc7bd63898304cf532
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-01-16 09:18:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.get("/", (req, res) => {
 // app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }))
 app.use(
   cors({
     credentials: true,
